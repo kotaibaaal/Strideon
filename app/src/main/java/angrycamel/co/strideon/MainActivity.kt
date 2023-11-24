1cfb6e534c4fdd7eb403b2bf5e09a52f31ad8b01
Kotaiba Aal <kotaiba.aal@gmail.com>
2023-11-24 14:04:29 +0100

    start

--- app/src/main/java/angrycamel/co/strideon/MainActivity.kt
+++ app/src/main/java/angrycamel/co/strideon/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "ABC $name!",
+            text = "Hi $name!",
             modifier = modifier
     )
 }
